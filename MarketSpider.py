49c625ef0ed60499608f9d347c6cce4f09038354
boristown <boristown@gmail.com>
2019-04-08 17:17:10 +0800

    迭代8：提取前100币

--- MarketSpider.py
+++ MarketSpider.py
@@ -4,7 +4,7 @@ import re
 
 html_path = "HTML/"
 crypto_path = os.path.join(html_path, "*Crypto*.htm")
-crypto_pattern = r'<tr\si="(\d+)">.+?rank\sicon">(\d+)<.+?title="(.+?)".+?title="(.+?)".+?</tr>'
+crypto_pattern = r'<tr.+?rank\sicon">(\d+)<.+?title="(.+?)".+?title="(.+?)".+?pid-(\d+)-last.+?</tr>'
 dirs = glob.glob( crypto_path )
 
 for file_path in dirs:
@@ -13,4 +13,4 @@ for file_path in dirs:
     file_str = file.read()
     matchs_str = re.finditer(crypto_pattern,file_str,re.S)
     for match_str in matchs_str:
-        print( match_str.group(1) + "\t" + match_str.group(2)+ "\t" + match_str.group(4)+ "\t" + match_str.group(3)  )
+        print( match_str.group(4) + "\t" + match_str.group(1)+ "\t" + match_str.group(3)+ "\t" + match_str.group(2)  )
