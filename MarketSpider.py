b5b11430c1c6468f1945806a6836b38b536487d6
boristown <boristown@gmail.com>
2019-04-08 15:00:00 +0800

    迭代5：正则表达式提取加密货币HTML片段

--- MarketSpider.py
+++ MarketSpider.py
@@ -1,13 +1,16 @@
 import os
 import glob
+import re
 
 html_path = "HTML/"
-
 crypto_path = os.path.join(html_path, "*Crypto*.htm")
+crypto_pattern = r"<tr\si=(.+?)</tr>"
 dirs = glob.glob( crypto_path )
 
 for file_path in dirs:
     print( file_path )
     file = open( file_path, "r", encoding="utf-8" )
     file_str = file.read()
-    print(file_str)
+    matchs_str = re.findall(crypto_pattern,file_str,re.S)
+    for match_str in matchs_str:
+        print( match_str )
