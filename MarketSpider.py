4ff7472445bdbcb9b60433b014831c8ab9362973
boristown <boristown@gmail.com>
2019-04-08 13:46:33 +0800

    迭代4：读取加密货币HTML内容

--- MarketSpider.py
+++ MarketSpider.py
@@ -6,5 +6,8 @@ html_path = "HTML/"
 crypto_path = os.path.join(html_path, "*Crypto*.htm")
 dirs = glob.glob( crypto_path )
 
-for file in dirs:
-    print( file )
+for file_path in dirs:
+    print( file_path )
+    file = open( file_path, "r", encoding="utf-8" )
+    file_str = file.read()
+    print(file_str)
